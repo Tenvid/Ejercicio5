5b6765c11c797b766d08a5147c3b78d7c4fb20ad
artearc <alexruicha@gmail.com>
2022-11-28 19:20:16 +0100

    Revert "corrección formulario"
    This reverts commit bdeb6d6d25eea0bec2f0bf5c48b43fe3be66d1ca.

--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -17,12 +17,11 @@ namespace WindowsFormsApp1
             InitializeComponent();
         }
 
-        private void boton_Click(object sender, EventArgs e)
+        private void button1_Click(object sender, EventArgs e)
         {
             string textoTelegrama;
-            char tipoTelegrama = 'o';
+            char tipoTelegrama = ' ';
             int numPalabras = 0;
-            int numEspacio = 1;
             double coste;
             //Leo el telegrama
             textoTelegrama = txtTelegrama.Text;
@@ -31,24 +30,19 @@ namespace WindowsFormsApp1
                 tipoTelegrama = 'u';
             //Obtengo el número de palabras que forma el telegrama
             numPalabras = textoTelegrama.Length;
-            for (int i = 0; i < numPalabras; i++)
-            {
-                if (textoTelegrama[i] == ' ')
-                    numEspacio++;
-            }
             //Si el telegrama es ordinario
             if (tipoTelegrama == 'o')
-                if (numEspacio <= 10)
-                    coste = 2.5;
+                if (numPalabras <= 10)
+                    coste = 25;
                 else
-                    coste = 2.5 + 0.5 * (numEspacio - 10);
+                    coste = 0.5 * numPalabras;
             else
             //Si el telegrama es urgente
             if (tipoTelegrama == 'u')
-                if (numEspacio <= 10)
+                if (numPalabras <= 10)
                     coste = 5;
                 else
-                    coste = 5 + 0.75 * (numEspacio - 10);
+                    coste = 5 + 0.75 * (numPalabras - 10);
             else
                 coste = 0;
             txtPrecio.Text = coste.ToString() + " euros";
